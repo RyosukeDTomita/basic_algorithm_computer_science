bc15b1a58f7f1cd0239b89f9d9c46dec003a891d
RyosukeDTomita <d310151c2@gmail.com>
2024-03-04 10:01:00 +0900

    Add shell sort implementation in C and Python

---
+++ sort/shell_sort/shell_sort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARRAY_SIZE 20
+
+void shell_sort(int array[], int array_size);
+int* create_random_array(void);
+void print_array(int* array, int array_size);
+
+/**
+ * サイズ20のランダムな配列を作成
+ */
+int* create_random_array(void) {
+  // 長期的に使えるメモリ領域ヒープを確保
+  int* array = (int*)malloc(ARRAY_SIZE * sizeof(int));
+
+  srand(3);  // 乱数シード
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    // 長い乱数は見にくいので0~99の範囲で乱数を発生させる。
+    array[i] = rand() % 100;
+  };
+  return array;
+}
+
+/**
+ * shell sort
+ */
+void shell_sort(int* array, int array_size) {
+  int h = 1;
+  while (h < array_size / 9) {
+    h = h * 3 + 1;
+  }
+  while (h > 0) {
+    for (int i = 1; i < ARRAY_SIZE; i++) {
+      int tmp = array[i];
+      int j = i;
+      while (j > 0 && array[j - 1] > tmp) {
+        array[j] = array[j - 1];
+        j = j - h;
+      }
+      array[j] = tmp;
+    }
+    h = h / 3;
+  }
+}
+
+/**
+ * 1次元配列をprintする
+ */
+void print_array(int* array, int array_size) {
+  // 配列のサイズは配列全体のバイト数 / 要素一つのバイト数で求められる。
+  for (int i = 0; i < array_size; i++) {
+    printf("%d, ", array[i]);
+  }
+  printf("\n");
+}
+
+int main(void) {
+  // ポインタ変数として作成された配列の先頭の値が格納されたメモリの位置を受け取る。
+  int* random_array = create_random_array();
+  print_array(random_array, ARRAY_SIZE);
+
+  printf("-----shell, sort-----\n");
+
+  shell_sort(random_array, ARRAY_SIZE);
+  print_array(random_array, ARRAY_SIZE);
+
+  // メモリを開放する。
+  free(random_array);
+  return 0;
+}
---
+++ sort/shell_sort/shell_sort.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import random
+
+
+def shell_sort(array):
+    """
+    hを3n+1としてループを回す毎にhを3で割る(切り捨て)しつつ，挿入ソートと同様に2つの値を比較して並べることを繰り返す。
+    """
+    len_array = len(array)
+    h = 1
+
+    # h-sortの間隔hを決定
+    while h < len_array / 9:
+        h = h * 3 + 1  # 4
+
+    while h > 0:
+        for i in range(h, len_array):
+            j = i
+            while j >= h and array[j - h] > array[j]:
+                array[j], array[j - h] = array[j - h], array[j]
+                j -= h
+        h = int(h / 3)  # FIXME
+    return array
+
+
+def main():
+    random.seed(3)  # randomシードを指定
+    my_array = [random.randint(0, 99) for _ in range(20)]
+    print(my_array)
+    print("-----SHELL SORT-----")
+    print(shell_sort(my_array))
+
+
+if __name__ == "__main__":
+    main()
