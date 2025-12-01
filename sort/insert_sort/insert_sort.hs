919d0b83b25ab5cab55dcfc190e6922f622be281
RyosukeDTomita <d310151c2@gmail.com>
2025-12-01 21:09:06 +0900

    update document and add fold version insert sort by Haskell
    update document and add fold version insert sort by Haskell
    
    update document and add fold version insert sort by Haskell

--- sort/insert_sort/insert_sort.hs
+++ sort/insert_sort/insert_sort.hs
@@ -1,14 +1,14 @@
 -- 要素を正しい位置に挿入する関数
-insert :: Ord a => a -> [a] -> [a]
+insert :: (Ord a) => a -> [a] -> [a]
 insert e [] = [e] -- 空リストへの挿入
-insert e (x:xs)
-  | e <= x = e:x:xs -- eをxの左へ
-  | otherwise = x:insert e xs -- eをxsの適切な位置へ挿入
+insert e (x : xs)
+  | e <= x = e : x : xs -- eをxの左へ
+  | otherwise = x : insert e xs -- eをxsの適切な位置へ挿入
 
 -- 挿入ソート関数
-insertSort :: Ord a => [a] -> [a]
+insertSort :: (Ord a) => [a] -> [a]
 insertSort [] = []
-insertSort (x:xs) = insert x (insertSort xs)
+insertSort (x : xs) = insert x (insertSort xs)
 
 main :: IO ()
 main = do
