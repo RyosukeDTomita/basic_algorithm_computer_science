6dd0683eef80647592888a65fd0ec382622caf29
RyosukeDTomita <d310151c2@gmail.com>
2026-02-11 21:01:40 +0900

    feat: fermat's little theorem

--- math/README.md
+++ math/README.md
@@ -16,8 +16,25 @@
 
 [eratosthenes](./eratosthenes/)
 
+### 実装例
+
 - [] C
 - [] Python
 - [x] Haskell
 
 ---
+
+## フェルマーの小定理
+
+フェルマーの小定理を利用して法的逆元(逆数)を求めるアルゴリズム
+
+a^(p-1) ≡ 1 (mod p) であることから、
+a * a^(p-2) ≡ 1 (mod p) となり、a^(p-2) が a の法的逆元となる。
+
+[fermat](./fermat/)
+
+### 実装例
+
+- [] C
+- [] Python
+- [x] Haskell
---
+++ math/fermat/fermat.hs
@@ -0,0 +1,31 @@
+import Data.Int (Int64)
+
+-- 法となる素数: 32 bit整数の最大値である2.1 * 10^9に近い
+modulus :: Int64
+modulus = 10 ^ 9 + 7
+
+-- 繰り返し二乗法による (n^k) mod modulus の計算
+-- e.g. x^9の場合: x^2 = x * x
+-- x^4 = x^2 * x^2
+-- x^8 = x^4 * x^4
+-- x^9 = x * x^8
+powMod :: Int64 -> Int64 -> Int64
+powMod _ 0 = 1
+powMod n k
+  | even k =
+      let half = powMod n (k `div` 2)
+       in (half * half) `mod` modulus
+  | otherwise = (n * powMod n (k - 1)) `mod` modulus
+
+-- フェルマーの小定理を用いた逆元計算
+-- a^(p-2) mod p
+invMod :: Int64 -> Int64
+invMod 0 = error "inverse of 0"
+invMod a = powMod a (modulus - 2)
+
+main :: IO ()
+main = do
+  let a = 3
+  let invA = invMod a
+  print invA
+  print $ (a * invA) `mod` modulus -- フェルマーの小定理より1になるはず。
